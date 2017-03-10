f725243f4ae77dac3613859e5d125e28e3780915
stovermc <stovermc@gmail.com>
2017-03-10 08:10:11 -0700

    refactored. final commit

--- app/models/task.rb
+++ app/models/task.rb
@@ -15,21 +15,19 @@ class Task
   def save
     @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
   end
-  #
-  # def self.database
-  #   database
-  # end
 
-  def self.all
+  def self.database
     database = SQLite3::Database.new('db/task_manager_development.db')
     database.results_as_hash = true
+    database
+  end
+
+  def self.all
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map { |task| Task.new(task) }
   end
 
   def self.find(id)
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
     task = database.execute("SELECT * FROM tasks WHERE id = ?", id).first
     Task.new(task)
   end
