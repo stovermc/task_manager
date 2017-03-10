47657f08b24bc3a595b1ed2d669baff1c46e90a5
stovermc <stovermc@gmail.com>
2017-03-10 08:07:48 -0700

    complete. needs refactor

--- app/models/task.rb
+++ app/models/task.rb
@@ -13,21 +13,23 @@ class Task
   end
 
   def save
-    @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @titile, @description)
+    @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
   end
-
-  def self.database
-    database = SQLite3::Database.new('db/task_manager_development')
-    database.results_as_hash = true
-    database
-  end
+  #
+  # def self.database
+  #   database
+  # end
 
   def self.all
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map { |task| Task.new(task) }
   end
 
   def self.find(id)
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
     task = database.execute("SELECT * FROM tasks WHERE id = ?", id).first
     Task.new(task)
   end
